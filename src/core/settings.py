ae57174e15abd3b7fc61076aaef28df513139cd5
Emmanuel Izuchukwu <55630541+NobleSalt@users.noreply.github.com>
2020-11-24 23:46:09 +0100

    Removed

--- src/core/settings.py
+++ src/core/settings.py
@@ -24,7 +24,6 @@ DATABASES['default'].update(db_from_env)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'a1+^=88^)ig76@bf!c+4)x(*gduxqr1jkeqs$u63k3+3xghg_%'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
